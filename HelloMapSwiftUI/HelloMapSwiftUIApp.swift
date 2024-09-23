e8524bc5b19c3af77f2b7536ab16dd626614931b
Danny Shen <shenfive@gmail.com>
2024-09-23 11:01:42 +0800

    Initial Commit

---
+++ HelloMapSwiftUI/HelloMapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMapSwiftUIApp.swift
+//  HelloMapSwiftUI
+//
+//  Created by 申潤五 on 2024/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloMapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
